9dfaafc5c1bf17b6fd13c2dfc4af255876cd2715
hasangokce <kodyap@gmail.com>
2020-12-04 21:29:46 +0300

    4 files modified

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -5,7 +5,7 @@ export default  class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        // You will add a map method that renders a set of Track components
+        /* You will add a map method that renders a set of Track components */
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./Components/App/App";
+import "./style.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
