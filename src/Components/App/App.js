a1bb68f082aee50bbfac73d8b7fa5e31ce7976c0
hasangokce <kodyap@gmail.com>
2020-12-04 16:46:35 +0300

    2 files modified

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,8 +4,12 @@ import "./App.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>
+        Ja<span class="highlight">mmm</span>ing
+      </h1>
+      <div class="App">
+        <div class="App-playlist" />
+      </div>
     </div>
   );
 }
