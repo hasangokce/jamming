591b7721dd21d14810b9a214c7ababe9f840ca49
hasangokce <kodyap@gmail.com>
2020-12-04 19:15:50 +0300

    1 file modified

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,7 +6,9 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       searchResults: [
-        { name: "name1", artist: "artist1", album: "album1", id: "1" }
+        { name: "name1", artist: "artist1", album: "album1", id: "1" },
+        { name: "name2", artist: "artist2", album: "album2", id: "2" },
+        { name: "name3", artist: "artist3", album: "album3", id: "3" }
       ]
     };
   }
